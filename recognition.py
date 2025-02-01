e278d8cb646a16dfe366c24ca389fc68ab2773b0
Maksym <05643mak@gmail.com>
2025-02-01 10:44:39 +0100

    small fix

--- recognition.py
+++ recognition.py
@@ -1,10 +1,13 @@
 import face_recognition
 
 def get_faces_positions(image_path):
-    # Load the image from the file system
-    image = face_recognition.load_image_file(image_path)
+    try: 
+        # Load the image from the file system
+        image = face_recognition.load_image_file(image_path)
     
-    # Detect all face locations
-    face_locations = face_recognition.face_locations(image)
+        # Detect all face locations
+        face_locations = face_recognition.face_locations(image, model="hog")
     
-    return face_locations
+        return face_locations
+    except:
+        return []
